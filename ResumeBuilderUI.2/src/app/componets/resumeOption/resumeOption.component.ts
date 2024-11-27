aef1f36d6a224f56b9e800a99c686c3aba1894ad
Tarek Hisham Ahmed Fouad <tahmedfo@uccs.edu>
2024-11-26 19:08:23 -0700

    Some changes to componet and login HTML

--- ResumeBuilderUI.2/src/app/componets/resumeOption/resumeOption.component.ts
+++ ResumeBuilderUI.2/src/app/componets/resumeOption/resumeOption.component.ts
@@ -31,11 +31,12 @@ export class ResumeOptionComponent {
   ngOnInit(): void {
     // Get the user ID from the session storage
     const userLoggedIn = sessionStorage.getItem('userId');
-
-    // If there is no user ID in the session storage
-    if (userLoggedIn == '' || userLoggedIn == '-1' || userLoggedIn == null) {
-      this.router.navigate(['login']); // Go to login page if user is not logged in
+    if(userLoggedIn!=null)
+    {
+      // If there is no user ID in the session storage
+      if (userLoggedIn == '' || userLoggedIn == '-1' || userLoggedIn == null) {
+        this.router.navigate(['login']); // Go to login page if user is not logged in
+      }
     }
   }
 }
-
